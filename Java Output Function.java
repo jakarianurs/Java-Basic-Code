d36a305782a5ea6d3e92e3864d59fb79f2a89c42
Md Jakaria Nur <122019360+mdjakarianurs@users.noreply.github.com>
2024-06-01 18:40:01 +0600

    Create Java Output Function.java

---
+++ Java Output Function.java
@@ -0,0 +1,7 @@
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Md Jakaria Nur");
+    System.out.println("Software Design and Development Engineer.");
+    System.out.println("He is also Digital Marketing and Graphics Designing Engineer.");
+  }
+}
