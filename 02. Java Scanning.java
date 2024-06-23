75ccbca915ad2acda7092d42634da04668ed6782
Md Jakaria Nur <122019360+mdjakarianurs@users.noreply.github.com>
2024-06-23 10:26:30 +0600

    Create 02. Java Scanning.java

---
+++ 02. Java Scanning.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class AddTwoInt {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        int num1 = input.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int num2 = input.nextInt();
+        
+        int sum = num1 + num2;
+        System.out.println("The sum of the two numbers: " + sum);
+    }
+}
